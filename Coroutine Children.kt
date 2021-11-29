6ba88ddd98efc2d160584bc874ca5eab78fc2219
aakash-12605 <aakash-12605@csez.zohocorpin.com>
2021-11-29 15:44:07 +0530

    Coroutine Children

---
+++ Coroutine Children.kt
@@ -0,0 +1,46 @@
+package main.kotlin
+
+import kotlinx.coroutines.*
+
+suspend fun main() {
+
+    coroutineScope {
+/*
+// launch a coroutine to process some kind of incoming request
+        val request = launch {
+            // it spawns two other jobs
+            launch(Job()) {
+                println("job1: I run in my own Job and execute independently!")
+                delay(1000)
+                println("job1: I am not affected by cancellation of the request")
+            }
+            // and the other inherits the parent context
+            launch {
+                delay(100)
+                println("job2: I am a child of the request coroutine")
+                delay(1000)
+                println("job2: I will not execute this line if my parent request is cancelled")
+            }
+        }
+        delay(500)
+        request.cancel() // cancel processing of the request
+        delay(1000) // delay a second to see what happens
+        println("main: Who has survived request cancellation?")
+    }
+
+*/
+
+        // launch a coroutine to process some kind of incoming request
+        val request = launch {
+            repeat(3) { i -> // launch a few children jobs
+                launch {
+                    delay((i + 1) * 200L) // variable delay 200ms, 400ms, 600ms
+                    println("Coroutine $i is done")
+                }
+            }
+            println("request: I'm done and I don't explicitly join my children that are still active")
+        }
+        request.join() // wait for completion of the request, including all its children
+        println("Now processing of the request is complete")
+    }
+}
