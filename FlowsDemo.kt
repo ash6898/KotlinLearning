da5ea08f4c1b5af2d730aeefc52073ac9a563085
aakash-12605 <aakash-12605@csez.zohocorpin.com>
2021-11-29 21:48:07 +0530

    Flows Demo 2

--- FlowsDemo.kt
+++ FlowsDemo.kt
@@ -1,20 +1,17 @@
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 
-
 fun simple(): Flow<Int> = flow {
-    println("Flow started")
-    for (i in 1..3) {
+    for (i in 1..5) {
         delay(100)
+        println("Emitting $i")
         emit(i)
     }
 }
 
 fun main() = runBlocking<Unit> {
-    println("Calling simple function...")
-    val flow = simple()
-    println("Calling collect...$flow")
-    flow.collect { value -> println(value) }
-    println("Calling collect again...")
-    flow.collect { value -> println(value) }
+    withTimeoutOrNull(350) { // Timeout after 250ms
+        simple().collect { value -> println(value) }
+    }
+    println("Done")
 }
